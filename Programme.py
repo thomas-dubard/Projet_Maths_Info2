e94e81a3389cc70c2e08850dfe6d9daa2f25c63b
Nix PARDUS <55082878+NeofelisNebulosa@users.noreply.github.com>
2020-01-09 17:23:48 +0100

    Formalisation

--- Programme.py
+++ Programme.py
@@ -1,12 +1,12 @@
 import numpy as np
 
+t0 = 0.  # permet de changer facilement la condition initiale en temps
+n = 50  # permet de changer facilement le nombre d'itérations que l'on va effectuer sur toutes les méthodes
 
-t0 = 0.     #permet de changer facilement la condition initiale en temps
-n = 50      #permet de changer facilement le nombre d'itérations que l'on va effectuer sur toutes les méthodes
 
-##Euler explicite
+# Euler explicite
 def solve_euler_explicit(f, x0, dt):
-    t = [t0]     #on va prendre la convention que la condition initiale x0 est en t0 qui est global sur tout le programme
+    t = [t0]  # on va prendre la convention que la condition initiale x0 est en t0 qui est global sur tout le programme
     x = [x0]
     for _ in range(n):
         x.append(x[-1] + dt * f(t[-1], x[-1]))
@@ -15,10 +15,12 @@ def solve_euler_explicit(f, x0, dt):
     x = np.array(x)
     return t, x
 
-#il faut ensuite tester cette fonction sur une équa diff facile à résoudre et montrer la convergence du schéma à l'ordre 1
+# il faut ensuite tester cette fonction sur une équa diff facile à résoudre et montrer la convergence du schéma à l'ordre 1
 
-##Calcul approximatif de la convergence à partir d'éléments issus de mon TIPE
-#Cette fonction calcule l'esperance d'une liste de valeurs pour permettre plus tard la realisation d'une regression lineaire.
+# Calcul approximatif de la convergence à partir d'éléments issus de mon TIPE
+# Cette fonction calcule l'esperance d'une liste de valeurs pour permettre plus tard la realisation d'une regression lineaire.
+
+
 def esperance(X):
     s = 0
     N = len(X)
@@ -26,17 +28,22 @@ def esperance(X):
         s += x
     return s / N
 
-#Cette fonction calcule la variance d'une liste de valeurs pour permettre plus tard la realisation d'une regression lineaire.
+
+# Cette fonction calcule la variance d'une liste de valeurs pour permettre plus tard la realisation d'une regression lineaire.
 def variance(X):
     X2 = [x**2 for x in X]
     return esperance(X2) - (esperance(X)**2)
 
-#Cette fonction calcule la covariance de deux listes de valeurs de meme taille pour permettre plus tard la realisation d'une regression lineaire
+
+# Cette fonction calcule la covariance de deux listes de valeurs de meme taille pour permettre plus tard la realisation d'une regression lineaire
+
 def covariance(X, Y):
     X_Y = [X[k]*Y[k] for k in range(len(X))]
     return esperance(X_Y) - esperance(X)*esperance(Y)
 
-#Cette fonction realise la regression lineaire d'une nuage de points dont les abscisses sont les valeurs de la liste X et les ordonnees celles de la liste Y. Elle renvoie la pente a et l'ordonnee a l'origine b.
+
+# Cette fonction realise la regression lineaire d'une nuage de points dont les abscisses sont les valeurs de la liste X et les ordonnees celles de la liste Y. Elle renvoie la pente a et l'ordonnee a l'origine b.
+
 def regression_lineaire(X, Y):
     e_x = esperance(X)
     e_y = esperance(Y)
@@ -46,8 +53,6 @@ def regression_lineaire(X, Y):
     b = - (a * e_x) + e_y
     return a, b
 
-#ensuite, on calcule l'erreur sur le dernier terme/l'aire et on fait une régression linéaire en log
+# ensuite, on calcule l'erreur sur le dernier terme/l'aire et on fait une régression linéaire en log
 
-#puis sur la meme courbe on ajoute le résultat qu'on aurait eu pour une methode d'ordre 2
-
-##
+# puis sur la meme courbe on ajoute le résultat qu'on aurait eu pour une methode d'ordre 2
