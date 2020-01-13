b46b79644edaf70db526714d1c6bd963a226bb0d
Nix PARDUS <55082878+NeofelisNebulosa@users.noreply.github.com>
2020-01-13 12:33:24 +0100

    Euler 1

--- Programme.py
+++ Programme.py
@@ -1,27 +1,38 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import math
 
 t0 = 0.  # permet de changer facilement la condition initiale en temps
+x0 = 1.
+eps = 1e-3
 n = 50  # permet de changer facilement le nombre d'itérations que l'on va effectuer sur toutes les méthodes
 
 
-# Euler explicite
-def solve_euler_explicit(f, x0, dt):
+# Euler explicite d'ordre 1
+def solve_euler_explicit(f, x0, dt=0.01, t0=0, tf=1):
     t = [t0]  # on va prendre la convention que la condition initiale x0 est en t0 qui est global sur tout le programme
     x = [x0]
-    for _ in range(n):
+    while t[-1] < tf and x[-1] < 100:
         x.append(x[-1] + dt * f(t[-1], x[-1]))
         t.append(t[-1] + dt)
-    t = np.array(t)
-    x = np.array(x)
+    t = np.array(t[:-1])
+    x = np.array(x[:-1])
     return t, x
 
-# il faut ensuite tester cette fonction sur une équa diff facile à résoudre et montrer la convergence du schéma à l'ordre 1
 
-# Calcul approximatif de la convergence à partir d'éléments issus de mon TIPE
+# Euler explicite d'ordre 1
+def solve_euler_explicit_2(f, x0, dt=0.01, t0=0, tf=1):
+    t = [t0]  # on va prendre la convention que la condition initiale x0 est en t0 qui est global sur tout le programme
+    x = [x0]
+    while t[-1] < tf and x[-1] < 100:
+        x.append(x[-1] + dt * f(t[-1], x[-1]))
+        t.append(t[-1] + dt)
+    t = np.array(t[:-1])
+    x = np.array(x[:-1])
+    return t, x
+
 
 # Cette fonction calcule l'esperance d'une liste de valeurs pour permettre plus tard la realisation d'une regression lineaire.
-
-
 def esperance(X):
     s = 0
     N = len(X)
@@ -51,6 +62,23 @@ def regression_lineaire(X, Y):
     a = cov_xy / v_x
     b = - (a * e_x) + e_y
     return a, b
+
+
+# il faut ensuite tester cette fonction sur une équa diff facile à résoudre et montrer la convergence du schéma à l'ordre 1
+def f_test(t, x):
+    return math.exp(x)
+
+
+dt = [3*1e-2, 1e-2, 1e-3, 1e-4]
+for i in range(len(dt)):
+    t_test, x_test = solve_euler_explicit(f_test, x0, dt[i], t0, t0 + 1)
+    x_real = np.exp(t_test)
+    plt.subplot(100*len(dt)+i+11)
+    plt.plot(t_test, x_test, color='red', label='Values simulated')
+    plt.plot(t_test, x_real, color='blue', label='Values calculated')
+plt.show()
+
+# Calcul approximatif de la convergence à partir d'éléments issus de mon TIPE
 
 # ensuite, on calcule l'erreur sur le dernier terme/l'aire et on fait une régression linéaire en log
 
