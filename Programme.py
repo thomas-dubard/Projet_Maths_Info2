a5f81f7cfed0b8243f0b400787f4afbe075dfa12
Nix PARDUS <55082878+NeofelisNebulosa@users.noreply.github.com>
2020-01-14 19:14:16 +0100

    Relire 01141914

--- Programme.py
+++ Programme.py
@@ -1,3 +1,4 @@
+from math import log
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -5,12 +6,12 @@ import math
 t0 = 0.  # permet de changer facilement la condition initiale en temps
 x0 = 1.
 eps = 1e-3
-n = 50  # permet de changer facilement le nombre d'itérations que l'on va effectuer sur toutes les méthodes
+n = 50  # permet de changer facilement le nombre d'itérations
 
 
 # Euler explicite d'ordre 1
 def solve_euler_explicit(f, x0, dt=0.01, t0=0, tf=1):
-    t = [t0]  # on va prendre la convention que la condition initiale x0 est en t0 qui est global sur tout le programme
+    t = [t0]  # la condition initiale x0 est en t0 qui est global
     x = [x0]
     while t[-1] < tf and x[-1] < 100:
         x.append(x[-1] + dt * f(t[-1], x[-1]))
@@ -22,8 +23,9 @@ def solve_euler_explicit(f, x0, dt=0.01, t0=0, tf=1):
 
 # Euler explicite d'ordre 2: méthode de Heun
 def solve_heun_explicit(f, x0, dt=0.01, t0=0, tf=1):
-    t = [t0]  
-    # on va prendre la convention que la condition initiale x0 est en t0 qui est global sur tout le programme
+    t = [t0]
+    # on va prendre la convention que la condition initiale x0 est en t0 qui
+    # est global sur tout le programme
     x = [x0]
     while t[-1] < tf:
         swap = f(t[-1], x[-1]) + f(t[-1], x[-1] + dt * f(t[-1], x[-1]))
@@ -34,7 +36,8 @@ def solve_heun_explicit(f, x0, dt=0.01, t0=0, tf=1):
     return t, x
 
 
-# Cette fonction calcule l'esperance d'une liste de valeurs pour permettre plus tard la realisation d'une regression lineaire.
+# Cette fonction calcule l'esperance d'une liste de valeurs pour
+# permettre plus tard la realisation d'une regression lineaire.
 def esperance(X):
     s = 0
     N = len(X)
@@ -43,19 +46,23 @@ def esperance(X):
     return s / N
 
 
-# Cette fonction calcule la variance d'une liste de valeurs pour permettre plus tard la realisation d'une regression lineaire.
+# Cette fonction calcule la variance d'une liste de valeurs pour
+# permettre plus tard la realisation d'une regression lineaire.
 def variance(X):
     X2 = [x**2 for x in X]
     return esperance(X2) - (esperance(X)**2)
 
 
-# Cette fonction calcule la covariance de deux listes de valeurs de meme taille pour permettre plus tard la realisation d'une regression lineaire
+# Cette fonction calcule la covariance de deux listes de valeurs de
+# meme taille pour permettre plus tard la realisation d'une regression lineaire
 def covariance(X, Y):
     X_Y = [X[k]*Y[k] for k in range(len(X))]
     return esperance(X_Y) - esperance(X)*esperance(Y)
 
 
-# Cette fonction realise la regression lineaire d'une nuage de points dont les abscisses sont les valeurs de la liste X et les ordonnees celles de la liste Y. Elle renvoie la pente a et l'ordonnee a l'origine b.
+# Cette fonction realise la regression lineaire d'une nuage de points dont
+# les abscisses sont les valeurs de la liste X et les ordonnees celles de
+# la liste Y. Elle renvoie la pente a et l'ordonnee a l'origine b.
 def regression_lineaire(X, Y):
     e_x = esperance(X)
     e_y = esperance(Y)
@@ -66,7 +73,8 @@ def regression_lineaire(X, Y):
     return a, b
 
 
-# il faut ensuite tester cette fonction sur une équa diff facile à résoudre et montrer la convergence du schéma à l'ordre 1
+# il faut ensuite tester cette fonction sur une équa diff facile à résoudre
+# et montrer la convergence du schéma à l'ordre 1
 def f_test(t, x):
     return x
 
@@ -81,7 +89,6 @@ for i in range(len(dt)):
     plt.plot(t_test, x_real, color='blue', label='Values calculated')
 plt.show()
 
-from math import log
 
 dt = [3*1e-2, 1e-2, 5*1e-3, 2*1e-3, 1e-3]
 err = []
@@ -90,12 +97,16 @@ for i in range(len(dt)):
     err.append(x_test[-1])
 plt.plot([-log(x, 10) for x in dt], err, color='red', label='Values simulated')
 a, b = regression_lineaire([-log(x, 10) for x in dt], err)
-plt.plot([-log(x, 10) for x in dt] , [-a * log(x, 10) + b for x in dt], color='blue', label='simulated')
+plt.plot([-log(x, 10) for x in dt],
+         [-a * log(x, 10) + b for x in dt],
+         color='blue', label='simulated')
 plt.show()
 print(a)
 
 # Calcul approximatif de la convergence à partir d'éléments issus de mon TIPE
 
-# ensuite, on calcule l'erreur sur le dernier terme/l'aire et on fait une régression linéaire en log
+# ensuite, on calcule l'erreur sur le dernier terme/l'aire et on fait une
+# régression linéaire en log
 
-# puis sur la meme courbe on ajoute le résultat qu'on aurait eu pour une methode d'ordre 2
+# puis sur la meme courbe on ajoute le résultat qu'on aurait eu pour
+# une methode d'ordre 2
